01bd6acef91057070dcb8e5c8a818efb0896e1fc
张武超 <1926321495@qq.com>
2022-01-03 22:40:28 +0800

    基础信息功能-基础视图信息

--- witmedProject/witmed-backend/witmed-commons/src/main/java/com/whackon/witmed/base/pojo/vo/BaseVO.java
+++ witmedProject/witmed-backend/witmed-commons/src/main/java/com/whackon/witmed/base/pojo/vo/BaseVO.java
@@ -7,27 +7,27 @@ import java.util.Date;
 /**
  * <b>基础信息功能-基础视图信息</b>
  * <p>
- *     基础视图信息设置如下属性
- *     1、status：系统状态<br/>
- *     2、createdBy：创建人<br/>
- *     3、createTime：创建时间<br/>
- *     4、modifiedBy：修改人<br/>
- *     5、modifiedTime：修改时间<br/>
- *     本项目所有功能视图信息<b>都必须继承于本基础视图信息</b>
+ * 基础视图信息设置如下属性
+ * 1、status：系统状态<br/>
+ * 2、createdUser：创建人<br/>
+ * 3、createTime：创建时间<br/>
+ * 4、updateUser：修改人<br/>
+ * 5、updateTime：修改时间<br/>
+ * 本项目所有功能视图信息<b>都必须继承于本基础视图信息</b>
  * </p>
  *
+ * @version 1.0.0
  * @Author 张武超
  * @Date 2021/12/29 23:30
  * @serial 1.0.0
- * @version 1.0.0
  */
 public class BaseVO implements Serializable {
 	private static final long serialVersionUID = 5307507834353921384L;
 	private String status;                  //系统状态：0-禁用，1-启用
-	private String createdBy;               //创建人
+	private String createdUser;             //创建人
 	private Date createTime;                //创建时间
-	private String modifiedBy;              //修改人
-	private Date modifiedTime;              //修改时间
+	private String updateUser;              //修改人
+	private Date updateTime;                //修改时间
 
 	public String getStatus() {
 		return status;
@@ -37,12 +37,12 @@ public class BaseVO implements Serializable {
 		this.status = status;
 	}
 
-	public String getCreatedBy() {
-		return createdBy;
+	public String getCreatedUser() {
+		return createdUser;
 	}
 
-	public void setCreatedBy(String createdBy) {
-		this.createdBy = createdBy;
+	public void setCreatedUser(String createdUser) {
+		this.createdUser = createdUser;
 	}
 
 	public Date getCreateTime() {
@@ -53,19 +53,19 @@ public class BaseVO implements Serializable {
 		this.createTime = createTime;
 	}
 
-	public String getModifiedBy() {
-		return modifiedBy;
+	public String getUpdateUser() {
+		return updateUser;
 	}
 
-	public void setModifiedBy(String modifiedBy) {
-		this.modifiedBy = modifiedBy;
+	public void setUpdateUser(String updateUser) {
+		this.updateUser = updateUser;
 	}
 
-	public Date getModifiedTime() {
-		return modifiedTime;
+	public Date getUpdateTime() {
+		return updateTime;
 	}
 
-	public void setModifiedTime(Date modifiedTime) {
-		this.modifiedTime = modifiedTime;
+	public void setUpdateTime(Date updateTime) {
+		this.updateTime = updateTime;
 	}
 }
