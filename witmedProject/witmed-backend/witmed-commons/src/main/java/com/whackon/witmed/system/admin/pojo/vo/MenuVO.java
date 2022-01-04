344cff16c97283a440562dd634e9dc8ea90d439d
张武超 <1926321495@qq.com>
2022-01-04 16:38:33 +0800

    系统功能 - 系统菜单功能视图信息

---
+++ witmedProject/witmed-backend/witmed-commons/src/main/java/com/whackon/witmed/system/admin/pojo/vo/MenuVO.java
@@ -0,0 +1,35 @@
+package com.whackon.witmed.system.admin.pojo.vo;
+
+import com.whackon.witmed.base.pojo.vo.BaseVO;
+import lombok.Data;
+
+/**
+ * <b>系统功能 - 系统菜单功能视图信息</b>
+ *<p>
+ *     系统菜单功能视图信息设置如下属性<br/>
+ *     1、id：主键<br/>
+ *     2、code：主键<br/>
+ *     3、parent：主键<br/>
+ *     4、text：主键<br/>
+ *     5、url：主键<br/>
+ *     6、icon：主键<br/>
+ *     7、sort：主键<br/>
+ *</p>
+ * @author 张武超
+ * @date 2022/1/4
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+@Data
+public class MenuVO extends BaseVO {
+	private static final long serialVersionUID = -3005713927290157590L;
+	private Long id;                    //主键
+	private String code;                //角色编码
+	private String parent;              //上级菜单
+	private String text;                //菜单文本
+	private String url;                 //链接地址
+	private String icon;                //菜单图标样式
+	private Long sort;                  //排序
+
+
+}
