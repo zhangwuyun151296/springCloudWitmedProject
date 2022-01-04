3877aa6df619d477c80ce1b38d8b04aeff1bb2cf
张武超 <1926321495@qq.com>
2022-01-04 16:36:07 +0800

    基础信息功能-系统响应编码枚举信息

--- witmedProject/witmed-backend/witmed-commons/src/main/java/com/whackon/witmed/base/pojo/enums/ResponseCode.java
+++ witmedProject/witmed-backend/witmed-commons/src/main/java/com/whackon/witmed/base/pojo/enums/ResponseCode.java
@@ -3,13 +3,13 @@ package com.whackon.witmed.base.pojo.enums;
 /**
  * <b>基础信息功能-系统响应编码枚举信息</b>
  * <p>
- *     系统响应编码枚举信息设置如下属性
+ *     系统响应编码枚举信息设置如下属性<br/>
  *     1、code：响应编码<br/>
  *     2、remarks：编码说明<br/>
- *        RESPONSE_SUCCESS：20000-系统业务处理完毕，响应成功
- *        RESPONSE_UNAUTH：30000-系统用户未进行认证
- *        RESPONSE_ERROR：40000-系统业务处理错误
- *        RESPONSE_EXCEPTION：50000-系统响异常
+ *        RESPONSE_SUCCESS：20000-系统业务处理完毕，响应成功<br/>
+ *        RESPONSE_UNAUTH：30000-系统用户未进行认证<br/>
+ *        RESPONSE_ERROR：40000-系统业务处理错误<br/>
+ *        RESPONSE_EXCEPTION：50000-系统响异常<br/>
  * </p>
  *
  * @Author 张武超
