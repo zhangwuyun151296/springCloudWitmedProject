75db40220ee75180e323b8d210868f9bc52bda8a
张武超 <1926321495@qq.com>
2022-01-04 16:41:04 +0800

    系统功能 - 医院部门视图信息

---
+++ witmedProject/witmed-backend/witmed-commons/src/main/java/com/whackon/witmed/system/hospital/pojo/vo/DepartmentVO.java
@@ -0,0 +1,29 @@
+package com.whackon.witmed.system.hospital.pojo.vo;
+
+import com.whackon.witmed.base.pojo.vo.BaseVO;
+import lombok.Data;
+
+/**
+ * <b>系统功能 - 医院部门视图信息</b>
+ * <p>
+ *     医院部门视图信息设置如下属性<br/>
+ *     1、id：主键<br/>
+ *     2、hospital：所在医院<br/>
+ *     3、parent：上级部门<br/>
+ *     4、code：部门编码<br/>
+ *     5、name：部门名称<br/>
+ * </p>
+ * @author 张武超
+ * @date 2022/1/4
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+@Data
+public class DepartmentVO extends BaseVO {
+	private static final long serialVersionUID = 5422723187036639884L;
+	private Long id;                    //主键
+	private String hospital;            //所在医院
+	private String parent;              //上级部门
+	private String code;                //部门编码
+	private String name;                //部门名称
+}
