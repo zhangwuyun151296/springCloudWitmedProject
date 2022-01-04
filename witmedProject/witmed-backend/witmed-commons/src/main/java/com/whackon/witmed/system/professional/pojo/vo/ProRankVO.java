810ac945c7e20ff1eefbc58a87856480585c3d27
张武超 <1926321495@qq.com>
2022-01-04 16:42:36 +0800

    系统功能 - 专业职务等级视图信息

---
+++ witmedProject/witmed-backend/witmed-commons/src/main/java/com/whackon/witmed/system/professional/pojo/vo/ProRankVO.java
@@ -0,0 +1,26 @@
+package com.whackon.witmed.system.professional.pojo.vo;
+
+import com.whackon.witmed.base.pojo.vo.BaseVO;
+import lombok.Data;
+
+/**
+ * <b>系统功能 - 专业职务等级视图信息</b>
+ * <p>
+ *     专业职务等级视图信息设置如下属性<br/>
+ *     1、id：主键<br/>
+ *     2、code：编码<br/>
+ *     3、name：名称<br/>
+ * </p>
+ * @author 张武超
+ * @date 2022/1/4
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+@Data
+public class ProRankVO extends BaseVO {
+	private static final long serialVersionUID = 7363309550345560124L;
+	private Long id;                    //主键
+	private String code;                //编码
+	private String name;                //名称
+
+}
