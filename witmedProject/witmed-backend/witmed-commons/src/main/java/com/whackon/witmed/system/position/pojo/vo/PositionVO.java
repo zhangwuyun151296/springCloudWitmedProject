2eae086c8ae2596153724463b9e0f4b93a7fbeb3
张武超 <1926321495@qq.com>
2022-01-04 16:41:51 +0800

    系统功能 - 人员职务视图信息

---
+++ witmedProject/witmed-backend/witmed-commons/src/main/java/com/whackon/witmed/system/position/pojo/vo/PositionVO.java
@@ -0,0 +1,25 @@
+package com.whackon.witmed.system.position.pojo.vo;
+
+import com.whackon.witmed.base.pojo.vo.BaseVO;
+import lombok.Data;
+
+/**
+ * <b>系统功能 - 人员职务视图信息</b>
+ * <p>
+ *     人员职务视图信息设置如下属性<br/>
+ *     1、id：主键<br/>
+ *     2、code：编码<br/>
+ *     3、name：名称<br/>
+ * </p>
+ * @author 张武超
+ * @date 2022/1/4
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+@Data
+public class PositionVO extends BaseVO {
+	private static final long serialVersionUID = -1974383078298077986L;
+	private Long id;                    //主键
+	private String code;                //编码
+	private String name;                //名称
+}
